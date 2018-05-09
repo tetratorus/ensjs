8883aced33bc3263ebab763b36a49fa332021a04
lentan <lentan1029@gmail.com>
2018-05-10 01:29:35 +0800

    Migrate some tests to async await and es6 syntax

--- test/test.js
+++ test/test.js
@@ -1,118 +1,127 @@
-var ENS = require('../index.js');
+const ENS = require('../index.js');
+process.on('warning', e => console.warn(e.stack));
+const assert = require('assert');
+const async = require('async');
+const fs = require('fs');
+const solc = require('solc');
+const TestRPC = require('ganache-cli');
+const Web3 = require('web3');
 
-var assert = require('assert');
-var async = require('async');
-var fs = require('fs');
-var solc = require('solc');
-var TestRPC = require('ganache-cli');
-var Web3 = require('web3');
+const web3 = new Web3();
 
-var web3 = new Web3();
+let ens = null;
+let ensRoot = null;
+let accounts = null;
+let deployens = null;
 
-var ens = null;
-var ensRoot = null;
-var accounts = null;
-var deployens = null;
+const registryInterface = [{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"resolver","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"resolver","type":"address"}],"name":"setResolver","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"label","type":"bytes32"},{"name":"owner","type":"address"}],"name":"setSubnodeOwner","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"owner","type":"address"}],"name":"setOwner","outputs":[],"type":"function"}];
 
-var registryInterface = [{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"resolver","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"resolver","type":"address"}],"name":"setResolver","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"label","type":"bytes32"},{"name":"owner","type":"address"}],"name":"setSubnodeOwner","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"owner","type":"address"}],"name":"setOwner","outputs":[],"type":"function"}];
-
-describe('ENS', function() {
+describe('ENS', () => {
 	before(function(done) {
 		this.timeout(50000);
 		web3.setProvider(TestRPC.provider());
 		//web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-		web3.eth.getAccounts(function(err, acct) {
+		web3.eth.getAccounts(async (err, acct) => {
 			if (acct) accounts = acct;
-			var source = fs.readFileSync('test/ens.sol').toString();
-			var compiled = solc.compile(source, 1);
+			const source = fs.readFileSync('test/ens.sol').toString();
+			const compiled = solc.compile(source, 1);
 			assert.equal(compiled.errors, undefined);
-			var deployer = compiled.contracts[':DeployENS'];
-			var deployensContract = new web3.eth.Contract(JSON.parse(deployer.interface));
+			const deployer = compiled.contracts[':DeployENS'];
+			const deployensContract = new web3.eth.Contract(JSON.parse(deployer.interface));
 
 			// Deploy the contract
-			deployensContract.deploy({
+			const newContractInstance = await deployensContract.deploy({
 				data: deployer.bytecode
 			})
 			.send({
 				from: accounts[0],
 				gas: 4700000
 			})
-			.on('error', function(err) { assert.fail(err); })
-			.then(function(newContractInstance) {
-				deployens = newContractInstance;
+			.on('error', async (err) => { assert.fail(err); })
+			deployens = newContractInstance;
+			try {
 				if (deployens.options.address != undefined) {
-					deployens.methods.ens().call().then(function(value) {
-						ensRoot = value;
-						ens = new ENS(web3.currentProvider, ensRoot);
-						done();
-					}).catch(function(err) {
-						assert.fail(err);
-					})
+					const value = await deployens.methods.ens().call()
+					ensRoot = value;
+					ens = new ENS(web3.currentProvider, ensRoot);
+					done();
 				} else {
 					assert.fail("Contract address is null", contract);
 				}
-			})
-			.catch(function(err) { assert.fail(err); });
+			} catch (err) {
+				assert.fail(err);
+			}
 		});
 	});
 
-	describe('#resolve()', function() {
-		it('should get resolver addresses', function(done) {
-			ens.resolver('foo.eth').resolverAddress().then(function(addr) {
+	describe('#resolve()', () => {
+		it('should get resolver addresses', async () => {
+			try {
+				const addr = await ens.resolver('foo.eth').resolverAddress();
 				assert.notEqual(addr, '0x0000000000000000000000000000000000000000');
-				done();
-			}).catch(assert.ifError);
+				return;
+			} catch (err) {
+				assert.fail(err);
+			}
 		});
 
-		it('should resolve names', function(done) {
-			ens.resolver('foo.eth').addr()
-			.then(function(result) {
+		it('should resolve names', async () => {
+			try {
+				const result = await ens.resolver('foo.eth').addr()
 				assert.equal(result, deployens._address);
-				done();
-			}).catch(assert.ifError);
+				return
+			} catch (err) {
+				assert.fail(err);
+			}
 		});
 
-		it('should implement has()', function(done) {
+		it('should implement has()', async () => {
 			var resolver = ens.resolver('foo.eth');
-
-			Promise.all([
-				resolver.has(web3.utils.asciiToHex('addr'))
-				.then(function(result) {
-					assert.equal(result, true);
-				}),
-				resolver.has(web3.utils.asciiToHex('blah'))
-				.then(function(result) {
-					assert.equal(result, false);
-				})
-			]).catch(assert.fail).then(function(result) {done()});
+			try {
+				return await Promise.all([
+					async () => {
+						const result = await resolver.has(web3.utils.asciiToHex('addr'))
+						assert.equal(result, true);
+					},
+					async () => {
+						const result = resolver.has(web3.utils.asciiToHex('blah'))
+						assert.equal(result, false);
+					}
+				]);
+			} catch (err) {
+				assert.fail(err);
+			}
 		});
 
-		it('should error when the name record does not exist', function(done) {
-			ens.resolver('bar.eth').addr()
-			.catch(function(err) {
+		it('should error when the name record does not exist', async () => {
+			try {
+				const result = await ens.resolver('bar.eth').addr();
+				assert.fail();
+			} catch (err) {
 				assert.ok(err.toString().indexOf('invalid JUMP') != -1, err);
-				done();
-			});
+				return;
+			}
 		});
 
-		it('should error when the name does not exist', function(done) {
-			ens.resolver('quux.eth').addr()
-			.catch(function(err) {
+		it('should error when the name does not exist', async () => {
+			try {
+				const result = await ens.resolver('quux.eth').addr();
+				assert.fail();
+			} catch (err) {
 				assert.equal(err, ENS.NameNotFound);
-				done();
-			});
+				return;
+			}
 		});
 
-		it('should permit name updates', function(done) {
-			var resolver = ens.resolver('bar.eth')
-			resolver.setAddr('0x0000000000000000000000000000000000012345')
-			.then(function(result) {
-				return resolver.addr()
-				.then(function(result) {
-					assert.equal(result, '0x0000000000000000000000000000000000012345');
-					done();
-				});
-			});
+		it('should permit name updates', async () => {
+			try {
+				const resolver = ens.resolver('bar.eth')
+				await resolver.setAddr('0x0000000000000000000000000000000000012345');
+				const result = await resolver.addr();
+				assert.equal(result, '0x0000000000000000000000000000000000012345');
+			} catch (err) {
+				assert.fail(err);
+			}
 		});
 
 		it('should do reverse resolution', function(done) {
